6e67f56edd9df8593fdf41a380925ed1ae901a80
shah-cloud <shah.fahed@knoldus.in>
2019-10-23 16:06:32 +0530

    develope | Updated test of dashboard services

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,16 +9,16 @@ object Dependencies {
   val logback: ModuleID = "ch.qos.logback" % "logback-classic" % logbackClassicVersion
   val config: ModuleID = "com.typesafe" % "config" % "1.2.1"
   val playJson: ModuleID = "com.typesafe.play" %% "play-json" % "2.8.0-M5"
-  val slick = "com.typesafe.slick" %% "slick" % "3.3.2"
-  val mysqlConnec = "mysql" % "mysql-connector-java" % "8.0.17"
-  val slickDb = "com.typesafe.slick" %% "slick-hikaricp" % "3.3.2"
-  val akkaHttp = "com.typesafe.akka" %% "akka-http" % "10.1.10"
-  val akkaStream = "com.typesafe.akka" %% "akka-stream" % "2.5.23"
-  val akkaSprayJson = "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.10"
-  val slickTest2 = "com.typesafe.akka" %% "akka-http-testkit" % "10.1.10"
-  val slickTest3 = "com.typesafe.akka" %% "akka-stream-testkit" % "2.5.23"
-  val slickTest4 = "org.postgresql" % "postgresql" % "42.2.5"
-  val h2 = "com.h2database" % "h2" % "1.4.192"
+  val slick: ModuleID = "com.typesafe.slick" %% "slick" % "3.3.2"
+  val mysqlConnec: ModuleID = "mysql" % "mysql-connector-java" % "8.0.17"
+  val slickDb: ModuleID = "com.typesafe.slick" %% "slick-hikaricp" % "3.3.2"
+  val akkaHttp: ModuleID = "com.typesafe.akka" %% "akka-http" % "10.1.10"
+  val akkaStream: ModuleID = "com.typesafe.akka" %% "akka-stream" % "2.5.23"
+  val akkaSprayJson: ModuleID = "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.10"
+  val slickTest2: ModuleID = "com.typesafe.akka" %% "akka-http-testkit" % "10.1.10"
+  val slickTest3: ModuleID = "com.typesafe.akka" %% "akka-stream-testkit" % "2.5.23"
+  val slickTest4: ModuleID = "org.postgresql" % "postgresql" % "42.2.5"
+  val h2: ModuleID = "com.h2database" % "h2" % "1.4.192"
 
   /** Test libraries */
   val scalaTest: ModuleID = "org.scalatest" %% "scalatest" % scalaTestVersion
